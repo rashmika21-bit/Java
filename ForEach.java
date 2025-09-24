aeb002a38584be1036fcd8b6941522d1bf19bbac
Rashmika <ranarashmika21@gmail.com>
2025-09-24 17:19:54 +0530

    Update ForEach.java

--- ForEach.java
+++ ForEach.java
@@ -3,6 +3,8 @@ public class ForEach {
         int[] arr={1,2,3,4,5,6};
         for(int ele : arr){
             System.out.print(ele+2 + " "); 
+            //For each loop is only used to display the array. we can't make any changes to an array using for each loop.
         }
     }
 }
+
